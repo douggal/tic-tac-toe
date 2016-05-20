b636fc2e3f1448160dd02932402ab41a8fe277af
douggal <douggal@gmail.com>
2016-05-19 20:50:09 -0400

    Feature 1.1 fixed bug in code

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -15,7 +15,7 @@ namespace TicTacToe
             Console.WriteLine("Command line Tic Tac Toe");
             Console.WriteLine("Added this line with the master branch");
 
-            Console.WriteLine("Added this line with Feature1.1");
+            Console.WriteLine("Added this line with Feature 1.1 updated changes");
 
             Console.WriteLine("Press any key to continue and exit...");
             Console.ReadKey();
