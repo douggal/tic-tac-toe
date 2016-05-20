e671c7f51534a98b207f7ca4f75d57f9c24ca8e5
douggal <douggal@gmail.com>
2016-05-19 20:07:41 -0400

    Initial commit - created project in VS and added Git source control

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // future home of tic tac toe
+
+            Console.WriteLine("Command line Tic Tac Toe");
+        }
+    }
+}
