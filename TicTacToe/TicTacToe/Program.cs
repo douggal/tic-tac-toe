6e421cf068adb65046f026ee3ba5d27f3449842e
douggal <douggal@gmail.com>
2016-05-19 20:31:10 -0400

    Feature1 first commit

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -15,6 +15,9 @@ namespace TicTacToe
             Console.WriteLine("Command line Tic Tac Toe");
             Console.WriteLine("Added this line with the master branch");
 
+            Console.WriteLine("Added this line with Feature1");
+            Console.WriteLine("Added this line with Feature1 too");
+
             Console.WriteLine("Press any key to continue and exit...");
             Console.ReadKey();
         }
